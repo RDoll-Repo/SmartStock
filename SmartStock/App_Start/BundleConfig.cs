a058511f4cc99f1a84a8486cfd4c50034a05e4e9
Isaac Van Meter <ivanmeter28@gmail.com>
2022-03-16 22:21:07 -0400

    HUD update

--- SmartStock/App_Start/BundleConfig.cs
+++ SmartStock/App_Start/BundleConfig.cs
@@ -23,7 +23,6 @@ namespace SmartStock
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
